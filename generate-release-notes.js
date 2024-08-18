1a44e3576a8f11d34755c0e72c3771e9e4f13ed6
Independent Creator <independentcreator@protonmail.com>
2024-08-18 09:22:38 +0100

    Es module syntax

--- generate-release-notes.js
+++ generate-release-notes.js
@@ -1,7 +1,11 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const changelogPath = path.join(__dirname, 'CHANGELOG.md');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const changelogPath = path.join(__dirname, '..', 'CHANGELOG.md');
 const changelog = fs.readFileSync(changelogPath, 'utf8');
 
 const latestVersion = changelog.split('\n')[0].replace('# ', '');
